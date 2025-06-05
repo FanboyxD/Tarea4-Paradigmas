03ed5f0e05a594d994175b324964996e11b191e8
FanboyxD <48070782+FanboyxD@users.noreply.github.com>
2025-06-05 17:45:40 -0600

    Delete GameClient directory

--- GameClient/src/main/java/iquick/gameclient/Enemy.java
+++
@@ -1,9 +0,0 @@
-package iquick.gameclient;
-
-public class Enemy {
-    public String id;
-    public float x;
-    public float y;
-    public boolean isActive = true;
-    public String enemyType = "NORMAL";
-}
--- GameClient/src/main/java/iquick/gameclient/GameObserver.java
+++
@@ -1,5 +0,0 @@
-package iquick.gameclient;
-
-public interface GameObserver {
-    void onGameStateUpdate();
-}
