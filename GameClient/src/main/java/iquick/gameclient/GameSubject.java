d35a55637ed4a2fc0e62e27bb6872f3f0895373a
FanboyxD <48070782+FanboyxD@users.noreply.github.com>
2025-06-05 04:33:37 -0600

    Delete GameClient directory

--- GameClient/src/main/java/iquick/gameclient/GameSubject.java
+++
@@ -1,7 +0,0 @@
-package iquick.gameclient;
-
-public interface GameSubject {
-    void registerObserver(GameObserver observer);
-    void removeObserver(GameObserver observer);
-    void notifyObservers();
-}
